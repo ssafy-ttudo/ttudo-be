91b3ca57ae5fe4c46d13f9ec96344d747ca7a3b8
Sooyon Park <pksuyon1122@naver.com>
2024-12-09 23:17:50 +0900

    수연 토큰 생성 및 저장 관련해서 주석으로 추가해놓음-오타 수정

--- ttodo-be_merge1/mypage/views.py
+++ ttodo-be_merge1/mypage/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 # from rest_framework.authentication import TokenAuthentication
 # from rest_framework.decorators import authentication_classes
 
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
